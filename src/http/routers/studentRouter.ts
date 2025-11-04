3724ba3518bdb7b75844dda623a31c1cb035557f
Gustavo <gbgustavo172@gmail.com>
2025-11-04 19:32:03 -0300

    getStudentsData adicionado

--- src/http/routers/studentRouter.ts
+++ src/http/routers/studentRouter.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { registerStudentController } from "../controllers/student/registerStudentController";
 import { verifyAuthToken } from "../middlewares/verifyAuthToken";
+import { getStudentData } from "../controllers/student/getStudentData";
 
 export async function studentRouter(app: FastifyInstance) {
     app.route({
@@ -8,5 +9,11 @@ export async function studentRouter(app: FastifyInstance) {
         method:["POST"],
         preHandler:verifyAuthToken,
         handler:registerStudentController,
-    })
+    });
+    app.route({
+        url:"/get",
+        method: ["GET"],
+        preHandler:verifyAuthToken,
+        handler:getStudentData,
+    });
 }
