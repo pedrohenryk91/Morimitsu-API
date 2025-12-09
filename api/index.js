f94a5c57e50d94f4f6bfd9275c0b61a9a8d8ab82
pedrohenryk91 <ph0639263@gmail.com>
2025-12-09 15:02:52 -0300

    vercel deploy fix

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import { app } from "../dist/server.js";
+
+export default async function handler(req, res) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,3 @@
-import { HOST, NODE_ENV, PORT } from "./lib/env";
 import { app } from "./lib/fastify";
 
-const port = Number(PORT);
-
-app.listen({
-    host:HOST,
-    port,
-}, (err, path) => {
-    console.log(err || path);
-    console.log("Environment:" + NODE_ENV);
-})
+export { app };
